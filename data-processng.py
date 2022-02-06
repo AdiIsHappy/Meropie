3414769b5ae8c13491fe9fc28c65d9cff8cceb73
Aditya <b21083@students.iitmandi.ac.in>
2022-02-06 23:59:39 +0530

    fg

--- data-processng.py
+++ data-processng.py
@@ -1,4 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
+import numpy as np
 
-f1 = pandas.read_csv("Modified_DelCp.csv", usecols=[0,1], index_col= 0)
+
+sgn = lambda x : x/abs(x) if x!= 0 else 0
+# [2437860:]
+f1 = pd.read_csv("Modified_Eta-Aql.csv", usecols=[0,1])
+f1["diff"] = f1["Magnitude"].diff()
+print(f1["diff"])
+x = np.array([0]*len(f1["diff"]))
+for i in range(2,len(f1["diff"])):
+    a = sgn(f1["diff"][i-1])
+    b = sgn(f1["diff"][i])
+    if a != b:
+        x[i] = f1["JD"][i] - f1["JD"][i-1]
+print(sum(x[x!= 0])/len(x[x!=0]))
+lastWasMax = True
+# for i in f1["Magnitude"][2437860:]:
+    
--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import pandas
 import matplotlib.pyplot as plt
 
 f1 = pandas.read_csv("Modified_DelCp.csv", usecols=[0,1], index_col= 0)
-print(f1.dtypes)
-f1.plot()
+f1["Smooth"] = f1["Magnitude"].rolling(500).mean()
+f1["Magnitude"][2437860:].plot()
 plt.show()
 
