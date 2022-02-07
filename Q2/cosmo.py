dbbbaefc2e530758491373d2a82ae7a07d8a5fe2
adi232004 <96134730+adi232004@users.noreply.github.com>
2022-02-07 17:10:02 +0530

    Update cosmo.py
    added units controll

--- Q2/cosmo.py
+++ Q2/cosmo.py
@@ -1,14 +1,14 @@
 from astropy.cosmology import WMAP9 as cosmo
 from astropy import units as u
 
-# ref :https://www.youtube.com/watch?v=2ZJEdce_4Zo
 print()
 print("enter input in format ScaleFactor CurrentDistance(in million Light Years) Obsarvable_Distance(in million Light Years)")
 ScaleFactor, distance, observebableDistance = map(float, input().split())
 redShift = lambda R : R-1
-distance *= pow(10,6) * 3.06601e-7
-observebableDistance *= pow(10,6) * 3.06601e-7
-Hubblesconsst = cosmo.H(redShift(ScaleFactor))
+distance *= pow(10,6)  *u.lyr
+observebableDistance *= pow(10,6) * u.lyr
+Hubblesconsst = cosmo.H(redShift(ScaleFactor)).to(u.kilometer/(u.kilometer*u.second))
 velocity = Hubblesconsst*distance
-time = (observebableDistance - distance) / velocity 
+time = ((observebableDistance - distance) / velocity ).to(u.year)
 print(f"Remaining time for observations is : {time}")
+print()
